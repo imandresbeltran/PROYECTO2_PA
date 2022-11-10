ef37f2b793129c6f48827cfe03ec0f821ad1926f
Ssergio0 <sergio7salguero@gmail.com>
2022-11-10 12:27:59 -0600

    Creacion del menú principal

--- Proyecto2 _BODEGAGRID/Proyecto2 _BODEGAGRID/Proyecto2 _BODEGAGRID.cpp
+++ Proyecto2 _BODEGAGRID/Proyecto2 _BODEGAGRID/Proyecto2 _BODEGAGRID.cpp
@@ -1,8 +1,51 @@
 #include "pch.h"
-
+#include <exception>
+using namespace std;
 using namespace System;
 
 int main(array<System::String ^> ^args)
 {
-    return 0;
+	int opcion;
+	
+	try
+	{
+		do
+		{
+			Console::WriteLine("Elija una opcion:");
+			Console::WriteLine("1. Crear Bahía.\n2. Ver Bahía.\n3. Ver inventario.\n4. Salir.");
+			opcion = int::Parse(Console::ReadLine());
+			switch (opcion)
+			{
+			case 1:Console::WriteLine("Haz seleccionado crear bahía. Presione enter para continuar.");
+				Console::ReadKey();
+				Console::Clear();
+				break;
+
+
+			case 2:Console::WriteLine("Haz seleccionado ver bahía. Presione enter para continuar.");
+				Console::ReadKey();
+				Console::Clear();
+				break;
+
+
+			case 3:Console::WriteLine("Haz seleccionado ver inventario. Presione enter para continuar.");
+				Console::ReadKey();
+				Console::Clear();
+				break;
+
+
+			case 4:Console::WriteLine("Haz seleccionado salir. Presione enter para salir.");
+				Console::ReadKey();
+				Environment::Exit(0);
+			default:
+				break;
+			}
+
+		} while (true);
+	}
+	catch (const std::exception&)
+	{
+		
+	}
+	
 }
